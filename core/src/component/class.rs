0e8596f9acca303094a8d0f36b74116b86a39105
White-Green <43771790+White-Green@users.noreply.github.com>
2022-04-10 03:53:43 +0900

    add ComponentClass

--- core/src/component/class.rs
+++ core/src/component/class.rs
@@ -1,1 +1,28 @@
-pub struct ComponentClass {/* TODO */}
+use crate::component::instance::ComponentInstance;
+use crate::component::parameter::ParameterType;
+use std::collections::HashMap;
+
+pub struct ComponentClass {
+    generate_image: bool,
+    generate_audio: bool,
+    parameter_type: HashMap<String, ParameterType>,
+    processor: (), // TODO:処理系を詰めないとどういう構成にするか決まらないのでとりあえず無を置いておく
+}
+
+impl ComponentClass {
+    pub fn new() -> ComponentClass {
+        todo!()
+    }
+    pub fn generate_image(&self) -> bool {
+        self.generate_image
+    }
+    pub fn generate_audio(&self) -> bool {
+        self.generate_audio
+    }
+    pub fn parameter_type(&self) -> &HashMap<String, ParameterType> {
+        &self.parameter_type
+    }
+    pub fn instantiate(&self) -> ComponentInstance<(), ()> {
+        todo!()
+    }
+}

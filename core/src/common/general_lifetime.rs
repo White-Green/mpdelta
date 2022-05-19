d0bbc81657ee4d07636d3ff4977b0f022b13ff2c
White-Green <43771790+White-Green@users.noreply.github.com>
2022-05-19 14:38:26 +0900

    refactor for Parameter

--- core/src/common/general_lifetime.rs
+++ core/src/common/general_lifetime.rs
@@ -1,20 +1,11 @@
 pub trait AsGeneralLifetime<'a> {
     type GeneralLifetimeType: 'a;
-    fn into_general_lifetime(self) -> Self::GeneralLifetimeType;
 }
 
 impl<'a, 'b: 'a, T: 'a> AsGeneralLifetime<'a> for &'b T {
     type GeneralLifetimeType = &'a T;
-
-    fn into_general_lifetime(self) -> Self::GeneralLifetimeType {
-        self
-    }
 }
 
 impl<'a, 'b: 'a, T: 'a> AsGeneralLifetime<'a> for &'b mut T {
     type GeneralLifetimeType = &'a mut T;
-
-    fn into_general_lifetime(self) -> Self::GeneralLifetimeType {
-        self
-    }
 }
--- core/src/component/parameter.rs
+++ core/src/component/parameter.rs
@@ -13,61 +13,106 @@ use std::sync::RwLock;
 pub mod placeholder;
 pub mod value;
 
-pub enum ParameterType {
-    Image,
-    Audio,
-    Video,
-    File { extension_filter: Option<Box<[String]>> },
-    String { length_range: Option<Range<usize>> },
-    Boolean,
-    Integer { range: Option<Range<i64>> },
-    RealNumber { range: Option<Range<f64>> },
-    Vec2 { range: Option<Range<Vector2<f64>>> },
-    Vec3 { range: Option<Range<Vector3<f64>>> },
-    Dictionary(HashMap<String, ParameterType>),
-    ComponentClass {/* TODO */},
+pub trait ParameterValueType<'a> {
+    type Image: 'a;
+    type Audio: 'a;
+    type Video: 'a;
+    type File: 'a;
+    type String: 'a;
+    type Boolean: 'a;
+    type Integer: 'a;
+    type RealNumber: 'a;
+    type Vec2: 'a;
+    type Vec3: 'a;
+    type Dictionary: 'a;
+    type ComponentClass: 'a;
 }
 
-pub enum ParameterValueFixed {
-    Image(ImagePlaceholder),
-    Audio(AudioPlaceholder),
-    File(PathBuf),
-    String(String),
-    Boolean(bool),
-    Integer(i64),
-    RealNumber(f64),
-    Vec2(Vector2<f64>),
-    Vec3(Vector3<f64>),
-    Dictionary(HashMap<String, ParameterValue>),
-    ComponentClass(/* TODO */),
+pub enum Parameter<'a, Type: ParameterValueType<'a>> {
+    Image(Type::Image),
+    Audio(Type::Audio),
+    Video(Type::Video),
+    File(Type::File),
+    String(Type::String),
+    Boolean(Type::Boolean),
+    Integer(Type::Integer),
+    RealNumber(Type::RealNumber),
+    Vec2(Type::Vec2),
+    Vec3(Type::Vec3),
+    Dictionary(Type::Dictionary),
+    ComponentClass(Type::ComponentClass),
 }
 
-pub enum ParameterValue {
-    Image(ImagePlaceholder),
-    Audio(AudioPlaceholder),
-    File(TimeSplitValue<StaticPointer<RwLock<MarkerPin>>, PathBuf>),
-    String(TimeSplitValue<StaticPointer<RwLock<MarkerPin>>, String>),
-    Boolean(TimeSplitValue<StaticPointer<RwLock<MarkerPin>>, bool>),
-    Integer(TimeSplitValue<StaticPointer<RwLock<MarkerPin>>, i64>),
-    RealNumber(TimeSplitValue<StaticPointer<RwLock<MarkerPin>>, EasingValue<f64>>),
-    Vec2(TimeSplitValue<StaticPointer<RwLock<MarkerPin>>, EasingValue<Vector2<f64>>>),
-    Vec3(TimeSplitValue<StaticPointer<RwLock<MarkerPin>>, EasingValue<Vector3<f64>>>),
-    Dictionary(TimeSplitValue<StaticPointer<RwLock<MarkerPin>>, HashMap<String, ParameterValue>>),
-    ComponentClass(/* TODO */),
+pub struct Type;
+pub type ParameterType = Parameter<'static, Type>;
+
+impl<'a> ParameterValueType<'a> for Type {
+    type Image = ();
+    type Audio = ();
+    type Video = ();
+    type File = Option<Box<[String]>>;
+    type String = Option<Range<usize>>;
+    type Boolean = ();
+    type Integer = Option<Range<i64>>;
+    type RealNumber = Option<Range<f64>>;
+    type Vec2 = Option<Range<Vector2<f64>>>;
+    type Vec3 = Option<Range<Vector3<f64>>>;
+    type Dictionary = HashMap<String, Parameter<'a, Type>>;
+    type ComponentClass = ();
 }
 
-pub enum ParameterValueViewForFix<'a> {
-    Image(ImagePlaceholder),
-    Audio(AudioPlaceholder),
-    File(TimeSplitValueView<'a, StaticPointer<RwLock<MarkerPin>>, PathBuf, Immutable, Mutable>),
-    String(TimeSplitValueView<'a, StaticPointer<RwLock<MarkerPin>>, String, Immutable, Mutable>),
-    Boolean(TimeSplitValueView<'a, StaticPointer<RwLock<MarkerPin>>, bool, Immutable, Mutable>),
-    Integer(TimeSplitValueView<'a, StaticPointer<RwLock<MarkerPin>>, i64, Immutable, Mutable>),
-    RealNumber(TimeSplitValueView<'a, StaticPointer<RwLock<MarkerPin>>, EasingValue<f64>, Immutable, Mutable>),
-    Vec2(TimeSplitValueView<'a, StaticPointer<RwLock<MarkerPin>>, EasingValue<Vector2<f64>>, Immutable, Mutable>),
-    Vec3(TimeSplitValueView<'a, StaticPointer<RwLock<MarkerPin>>, EasingValue<Vector3<f64>>, Immutable, Mutable>),
-    Dictionary(TimeSplitValueView<'a, StaticPointer<RwLock<MarkerPin>>, HashMap<String, ParameterValue>, Immutable, Mutable>),
-    ComponentClass(/* TODO */),
+pub struct Value;
+pub type ParameterValue = Parameter<'static, Value>;
+
+impl<'a> ParameterValueType<'a> for Value {
+    type Image = ImagePlaceholder;
+    type Audio = AudioPlaceholder;
+    type Video = (ImagePlaceholder, AudioPlaceholder);
+    type File = TimeSplitValue<StaticPointer<RwLock<MarkerPin>>, PathBuf>;
+    type String = TimeSplitValue<StaticPointer<RwLock<MarkerPin>>, String>;
+    type Boolean = TimeSplitValue<StaticPointer<RwLock<MarkerPin>>, bool>;
+    type Integer = TimeSplitValue<StaticPointer<RwLock<MarkerPin>>, i64>;
+    type RealNumber = TimeSplitValue<StaticPointer<RwLock<MarkerPin>>, EasingValue<f64>>;
+    type Vec2 = TimeSplitValue<StaticPointer<RwLock<MarkerPin>>, EasingValue<Vector2<f64>>>;
+    type Vec3 = TimeSplitValue<StaticPointer<RwLock<MarkerPin>>, EasingValue<Vector3<f64>>>;
+    type Dictionary = TimeSplitValue<StaticPointer<RwLock<MarkerPin>>, HashMap<String, ParameterValue>>;
+    type ComponentClass = ();
+}
+
+pub struct ValueFixed;
+pub type ParameterValueFixed = Parameter<'static, ValueFixed>;
+
+impl<'a> ParameterValueType<'a> for ValueFixed {
+    type Image = ImagePlaceholder;
+    type Audio = AudioPlaceholder;
+    type Video = (ImagePlaceholder, AudioPlaceholder);
+    type File = PathBuf;
+    type String = String;
+    type Boolean = bool;
+    type Integer = i64;
+    type RealNumber = f64;
+    type Vec2 = Vector2<f64>;
+    type Vec3 = Vector3<f64>;
+    type Dictionary = HashMap<String, ParameterValue>;
+    type ComponentClass = ();
+}
+
+pub struct ValueViewForFix;
+pub type ParameterValueViewForFix<'a> = Parameter<'a, ValueViewForFix>;
+
+impl<'a> ParameterValueType<'a> for ValueViewForFix {
+    type Image = ImagePlaceholder;
+    type Audio = AudioPlaceholder;
+    type Video = (ImagePlaceholder, AudioPlaceholder);
+    type File = TimeSplitValueView<'a, StaticPointer<RwLock<MarkerPin>>, PathBuf, Immutable, Mutable>;
+    type String = TimeSplitValueView<'a, StaticPointer<RwLock<MarkerPin>>, String, Immutable, Mutable>;
+    type Boolean = TimeSplitValueView<'a, StaticPointer<RwLock<MarkerPin>>, bool, Immutable, Mutable>;
+    type Integer = TimeSplitValueView<'a, StaticPointer<RwLock<MarkerPin>>, i64, Immutable, Mutable>;
+    type RealNumber = TimeSplitValueView<'a, StaticPointer<RwLock<MarkerPin>>, EasingValue<f64>, Immutable, Mutable>;
+    type Vec2 = TimeSplitValueView<'a, StaticPointer<RwLock<MarkerPin>>, EasingValue<Vector2<f64>>, Immutable, Mutable>;
+    type Vec3 = TimeSplitValueView<'a, StaticPointer<RwLock<MarkerPin>>, EasingValue<Vector3<f64>>, Immutable, Mutable>;
+    type Dictionary = TimeSplitValueView<'a, StaticPointer<RwLock<MarkerPin>>, HashMap<String, ParameterValue>, Immutable, Mutable>;
+    type ComponentClass = ();
 }
 
 pub struct ImageRequiredParams {/* TODO */}
@@ -79,6 +124,7 @@ impl<'a> From<&'a mut ParameterValue> for ParameterValueViewForFix<'a> {
         match value {
             ParameterValue::Image(a) => ParameterValueViewForFix::Image(*a),
             ParameterValue::Audio(a) => ParameterValueViewForFix::Audio(*a),
+            ParameterValue::Video(a) => ParameterValueViewForFix::Video(*a),
             ParameterValue::File(a) => ParameterValueViewForFix::File(TimeSplitValueView::new(a)),
             ParameterValue::String(a) => ParameterValueViewForFix::String(TimeSplitValueView::new(a)),
             ParameterValue::Boolean(a) => ParameterValueViewForFix::Boolean(TimeSplitValueView::new(a)),
@@ -87,15 +133,11 @@ impl<'a> From<&'a mut ParameterValue> for ParameterValueViewForFix<'a> {
             ParameterValue::Vec2(a) => ParameterValueViewForFix::Vec2(TimeSplitValueView::new(a)),
             ParameterValue::Vec3(a) => ParameterValueViewForFix::Vec3(TimeSplitValueView::new(a)),
             ParameterValue::Dictionary(a) => ParameterValueViewForFix::Dictionary(TimeSplitValueView::new(a)),
-            ParameterValue::ComponentClass() => ParameterValueViewForFix::ComponentClass(),
+            ParameterValue::ComponentClass(a) => ParameterValueViewForFix::ComponentClass(*a),
         }
     }
 }
 
 impl<'a: 'b, 'b> AsGeneralLifetime<'b> for ParameterValueViewForFix<'a> {
     type GeneralLifetimeType = ParameterValueViewForFix<'b>;
-
-    fn into_general_lifetime(self) -> Self::GeneralLifetimeType {
-        self
-    }
 }

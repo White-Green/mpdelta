a91b8ccf00c51ed906e090b38cca0138685d1d2a
White-Green <43771790+White-Green@users.noreply.github.com>
2022-05-31 03:11:22 +0900

    define Parameter type for ComponentProcessorOutput

--- core/src/component/parameter/placeholder.rs
+++ core/src/component/parameter/placeholder.rs
@@ -1,3 +1,5 @@
+use crate::component::marker_pin::MarkerTime;
+use std::ops::Range;
 use uuid::Uuid;
 
 #[derive(Debug, Clone, Copy, Ord, PartialOrd, Eq, PartialEq, Hash)]
@@ -5,7 +7,19 @@ pub struct ImagePlaceholder {
     id: Uuid,
 }
 
+#[derive(Debug, Clone, Eq, PartialEq, Hash)]
+pub struct TimedImagePlaceholder {
+    id: ImagePlaceholder,
+    time: Vec<Option<Range<MarkerTime>>>,
+}
+
 #[derive(Debug, Clone, Copy, Ord, PartialOrd, Eq, PartialEq, Hash)]
 pub struct AudioPlaceholder {
     id: Uuid,
+}
+
+#[derive(Debug, Clone, Eq, PartialEq, Hash)]
+pub struct TimedAudioPlaceholder {
+    id: AudioPlaceholder,
+    time: Vec<Option<Range<MarkerTime>>>,
 }
--- core/src/component/processor.rs
+++ core/src/component/processor.rs
@@ -1,17 +1,40 @@
 use crate::common::mapped_slice::MappedSliceMut;
+use crate::common::time_split_value::TimeSplitValue;
 use crate::component::instance::ComponentInstance;
-use crate::component::parameter::{ParameterType, ParameterValue, ParameterValueFixed, ParameterValueViewForFix};
+use crate::component::marker_pin::MarkerTime;
+use crate::component::parameter::placeholder::{TimedAudioPlaceholder, TimedImagePlaceholder};
+use crate::component::parameter::value::EasingValue;
+use crate::component::parameter::{Parameter, ParameterType, ParameterValue, ParameterValueFixed, ParameterValueType, ParameterValueViewForFix};
+use cgmath::{Vector2, Vector3};
+use std::collections::HashMap;
+use std::path::PathBuf;
 use std::time::Duration;
 
 pub enum ComponentProcessorOutput {
     Native {
-        processor: (), // TODO: Nativeのprocessorが未定なので無を置いておく
-        parameter: Vec<ParameterValue>,
+        processor: (),
+        // TODO: Nativeのprocessorが未定なので無を置いておく
+        parameter: Vec<Parameter<'static, ComponentProcessorOutput>>,
     },
     Component {
         components: Vec<ComponentInstance>,
         link: (), // TODO: MarkerPin同士のLink
     },
+}
+
+impl<'a> ParameterValueType<'a> for ComponentProcessorOutput {
+    type Image = TimedImagePlaceholder;
+    type Audio = TimedAudioPlaceholder;
+    type Video = (TimedImagePlaceholder, TimedAudioPlaceholder);
+    type File = TimeSplitValue<MarkerTime, PathBuf>;
+    type String = TimeSplitValue<MarkerTime, String>;
+    type Boolean = TimeSplitValue<MarkerTime, bool>;
+    type Integer = TimeSplitValue<MarkerTime, i64>;
+    type RealNumber = TimeSplitValue<MarkerTime, EasingValue<f64>>;
+    type Vec2 = TimeSplitValue<MarkerTime, EasingValue<Vector2<f64>>>;
+    type Vec3 = TimeSplitValue<MarkerTime, EasingValue<Vector3<f64>>>;
+    type Dictionary = TimeSplitValue<MarkerTime, HashMap<String, ParameterValue>>;
+    type ComponentClass = ();
 }
 
 pub trait ComponentProcessor {

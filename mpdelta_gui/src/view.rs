d0ce47287dd74e8da66258c63c4a9539cc0b4f67
White-Green <43771790+White-Green@users.noreply.github.com>
2022-07-17 07:22:22 +0900

    update temporary GUI

--- mpdelta_gui/src/view.rs
+++ mpdelta_gui/src/view.rs
@@ -1,4 +1,4 @@
-use egui::{Context, Frame, ScrollArea};
+use egui::{Context, Frame, ScrollArea, Style};
 
 pub trait Gui {
     fn ui(&mut self, ctx: &Context);
@@ -14,20 +14,27 @@ impl MPDeltaGUI {
 
 impl Gui for MPDeltaGUI {
     fn ui(&mut self, ctx: &Context) {
-        egui::TopBottomPanel::top("menu_bar").show(ctx, |ui| {
-            ui.horizontal(|ui| {
-                if ui.button("File").clicked() {
-                    println!("File-clicked");
-                }
-                if ui.button("Edit").clicked() {
-                    println!("Edit-clicked");
-                }
+        egui::TopBottomPanel::top("menu").show(ctx, |ui| {
+            egui::menu::bar(ui, |ui| {
+                ui.menu_button("File", |ui| {
+                    if ui.button("Open").clicked() {
+                        println!("File/Open Clicked");
+                    }
+                });
+                ui.menu_button("Edit", |ui| {
+                    if ui.button("Undo").clicked() {
+                        println!("Edit/Undo Clicked");
+                    }
+                    if ui.button("Redo").clicked() {
+                        println!("Edit/Redo Clicked");
+                    }
+                });
             });
         });
 
         egui::TopBottomPanel::bottom("bottom_info_bar").show(ctx, |ui| {
             ui.horizontal(|ui| {
-                ui.label("mpdelta");
+                ui.label(concat!("mpdelta - ", env!("CARGO_PKG_VERSION")));
                 egui::warn_if_debug_build(ui);
             })
         });

2e97c8a0215b5ed34278b881a1c792a2231041b0
White-Green <43771790+White-Green@users.noreply.github.com>
2024-02-17 10:28:05 +0900

    test

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use async_trait::async_trait;
+use async_trait :: async_trait;
 use cpal::traits::HostTrait;
 use mpdelta_audio_mixer::MPDeltaAudioMixerBuilder;
 use mpdelta_components::rectangle::RectangleClass;

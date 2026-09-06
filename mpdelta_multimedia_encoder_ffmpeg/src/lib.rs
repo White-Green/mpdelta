516ba9a1fb25528c27c1d5180af3c9a7d0114ab5
White-Green <43771790+White-Green@users.noreply.github.com>
2026-09-06 11:51:01 +0900

    Refactor: Borrow `audio_receiver` for clarity and ensure consistent handling

--- mpdelta_multimedia_encoder_ffmpeg/src/lib.rs
+++ mpdelta_multimedia_encoder_ffmpeg/src/lib.rs
@@ -313,6 +313,7 @@ fn encode_thread<T: Write + Seek + Send + Sync + 'static>(
                 }
             }
         });
+        let audio_receiver = &audio_receiver;
         let mut audio_stream = audio_stream.map(|(id, mut encoder, options, variable_frame_size)| {
             let frame_size = if variable_frame_size { encoder.rate() / 20 } else { encoder.frame_size() };
             let mut f32_frame = frame::Audio::new(Sample::F32(Type::Planar), frame_size as usize, ChannelLayout::STEREO);
